3a830f3ca478cfb070d814bcf5f096b74674f82a
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-10 19:30:45 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+
+int main(void) {
+
+	/**
+	 * main - Entry point
+	 * description: sfdlkjfds
+	 * Return : always 0 
+ * /
+puts("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+return (0);
+}
