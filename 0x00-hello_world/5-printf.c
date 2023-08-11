99ea60d7a45a8c2274dc8aa4a49be808c5a9bc30
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-11 05:13:32 -0700

    A

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-/*
+/**
 *main - Entry point
 */
 int main(void)
+	/**
+	 * main Entry - point
+	 * return result (0)
+	 */
 {
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
