cda5fd2eee4805ff222dba49282bcba9875a361f
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-10 19:59:36 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
 
 int main(viod)
 {
-      printf("Size of a char: %lu bytes(s)" , sizof(char));
-      printf("Size of a int: %lu bytes(s)" , sizof(int));
-      printf("Size of a long int: %lu bytes(s)" , sizof(long int));
-      printf("Size of a long long int: %lu bytes(s)" , sizof(long long int));
-      printf("Size of a float: %lu bytes(s)" , sizof(float));
+      printf("Size of a char: %lu byte(s)\n" , sizof(char));
+      printf("Size of a int: %lu byte(s)\n" , sizof(int));
+      printf("Size of a long int: %lu byte(s)\n" , sizof(long int));
+      printf("Size of a long long int: %lu byte(s)\n" , sizof(long long int));
+      printf("Size of a float: %lu byte(s)\n" , sizof(float));
 return(0);
 }
