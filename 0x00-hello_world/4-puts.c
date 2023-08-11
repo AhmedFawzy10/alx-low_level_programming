3538c1f9e37e5aa7e9419462e16b7c7bde406950
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-10 23:53:58 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /**  * main - Entry point  *  * Return: Always 0 (Success)  */ 
 int main(void)
 {
-      	puts("\"Programming is like building a multilingual puzzle");  
+      	puts("\"Programming is like building a multilingual puzzle\n");  
 	return (0);
 }
