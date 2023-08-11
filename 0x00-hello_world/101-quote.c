d8547c487075f7d351d7589d3308b95d3fd6431b
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-11 06:08:41 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdoi.h>
+/*
+ * main Entry - point 
+ *
+ * return succes 0 (result)
+ */
+int main<void>
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return(1);
+}
