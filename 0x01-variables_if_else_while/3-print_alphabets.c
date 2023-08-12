f54b7c86437910393e4908e4c37ec03bf8dcc8dd
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-12 02:12:58 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>                     
-#include <time.h>                       
-#include <stdio.h>                      
-/**                                     
-* main - main entry                     
-* description:sa                        
-*                                       
-* Return: always 0                      
-*/                                      
-int main(void)                          
-{                                       
-char ch;                                
-for(ch = 'a'; ch <= 'Z'; "\n" ch++)         
-{                                       
-putchar(ch);                            
-}                                       
-return (0);                             
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - main entry
+* description:sa
+*
+* Return: always 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'Z' "\n";  ch++)
+{
+putchar(ch);
+}
+putcahr('\n');
+return (0);
 }
