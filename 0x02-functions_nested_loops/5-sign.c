1b3b0daa4d447b534c8a29fe27600c547bc5bf96
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-27 14:56:52 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,4 @@
-#include "main.h"
-
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-
-5-sign.c
-
-#include "main.h"
-
+include "main.h"
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
