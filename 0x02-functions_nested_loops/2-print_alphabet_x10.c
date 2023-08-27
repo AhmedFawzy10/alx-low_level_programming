86fcf6fda9859e9189b766d321ac2da051bbd5b4
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-27 10:50:08 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,22 +5,15 @@
 	 */
 void print_alphabet_x10(void)
 {
-
+int i = 0;
 char ch;
+while(i<10){
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
-putchar(ch);
 }
-
-
+putchar('\n');
+i++;
+}
 
 }
