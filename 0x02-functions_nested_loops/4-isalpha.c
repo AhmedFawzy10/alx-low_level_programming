be949c84b62dc7fd7bd3a449ce128662402c30ed
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-27 12:21:15 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,9 @@
-
 #include "main.h"
-
-
-	/**
-	 * _islower - check if char is lowercase
-	 * @c: is the char to be checked
-	 * Return: 1 if char is lowercase, otherwise 0.
-	 */
+/**
+ *_islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+*/
 int _isalpha(int c)
 {
 if (c >= 97 && c <= 122)
