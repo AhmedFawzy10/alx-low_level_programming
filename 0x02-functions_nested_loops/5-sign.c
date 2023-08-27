7e71f81449ee6953e02f92acc9a2352599152667
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-08-27 14:44:40 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+putchar('+');
 return (1);
 }
 else if (n == 0)
@@ -18,9 +18,9 @@ else if (n == 0)
 putchar(48);
 return (0);
 }
-else
+else if (n < 0)
 {
-putchar(45);
+putchar('-');
 return (-1);
 }
 }
