f825143e057dc0acff00c3bb4ed01e141148c062
AhmedFawzy10 <aaldsoaky10@gmail.com>
2023-09-03 08:39:33 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
 int print_last_digit(int n)
 {
 int last_digit;
-last_digit = (n % 10);
 if (n < 0)
-{
-last_digit = -last_digit;
-}
-putchar(last_digit + '0');
+
+last_digit = -1 * (n % 10);
+else
+	last_digit = n % 10;
+
+_putchar(last_digit + '0');
 return (last_digit);
 }
